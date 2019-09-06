794a4ac05d95d2198e1caea3a108e8c8a667df37
Sumanth Pai <52402733+SumanthPaiJS@users.noreply.github.com>
2019-09-06 10:46:38 +0530

    actions update

---
+++ src/components/actions/tickets.js
@@ -0,0 +1,16 @@
+export const addTicket = (ticket) => {
+    return {
+        type: 'ADD_TICKET', 
+        payload: ticket 
+    }
+}
+
+// removeTicket 
+export const removeTicket = (id) => {
+    return {
+        type: 'REMOVE_TICKET',
+        payload: id 
+    }
+}
+
+// editTicket 
